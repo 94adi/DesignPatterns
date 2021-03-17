36219f60fb78c5df3b8fd2127de14ba4a9e6f563
94adi <adrianrcotuna@gmail.com>
2021-03-18 00:08:52 +0200

    Per thread singleton

---
+++ Singleton/PerThreadSingleton/PerThreadSingleton/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PerThreadSingleton
+{
+    public sealed class RequestHandler
+    {
+        private static ThreadLocal<RequestHandler> threadInstance
+          = new ThreadLocal<RequestHandler>(
+            () => new RequestHandler());
+
+        public int Id;
+
+        private RequestHandler()
+        {
+            Id = Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public void Handle(string request)
+        {
+            Console.WriteLine($"Handling {request}");
+        }
+
+        public static RequestHandler Instance => threadInstance.Value;
+    }
+
+    public class Demo
+    {
+        public static void Main(string[] args)
+        {
+            var t1 = Task.Factory.StartNew(() =>
+            {
+                var requestHandler = RequestHandler.Instance;
+                Console.WriteLine($"t1: " + requestHandler.Id);
+                requestHandler.Handle("http://test.com");
+            });
+            var t2 = Task.Factory.StartNew(() =>
+            {
+                var requestHandler = RequestHandler.Instance;
+                Console.WriteLine($"t2: " + requestHandler.Id);
+                Console.WriteLine($"t2 again: " + requestHandler.Id);
+                requestHandler.Handle("http://test2.com");
+            });
+            Task.WaitAll(t1, t2);
+        }
+
+
+    }
+}
