79eb811269f087ca5959a0127b655fae5557cccc
94adi <adrianrcotuna@gmail.com>
2021-06-21 22:22:43 +0300

    Presentation 2

---
+++ Proxy/Presentation/DynamicLoggerExamples/DuckTyping/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DuckTyping
+{
+    class Duck
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Duck quacks");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Duck swims");
+        }
+    }
+
+    class MallardDuck
+    {
+        public void Quack()
+        {
+            Console.WriteLine("MallardDuck quacks");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("MallardDuck swims");
+        }
+        
+        public void Fly()
+        {
+            Console.WriteLine("MallardDuck flies");
+        }
+    }
+
+    class Program
+    {
+        static void CallADuck(dynamic myDuck)
+        {
+            myDuck.Quack();
+            myDuck.Swim();
+        }
+
+        static void Main(string[] args)
+        {
+            CallADuck(new Duck());
+            CallADuck(new MallardDuck());
+        }
+    }
+}
