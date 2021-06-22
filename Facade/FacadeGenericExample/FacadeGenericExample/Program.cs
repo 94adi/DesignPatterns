4b9310b635be167d150be80069e788e217e46c34
Adrian Cotuna <adrianrcotuna@gmail.com>
2021-06-22 17:37:35 +0300

    Facade pattern

---
+++ Facade/FacadeGenericExample/FacadeGenericExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FacadeGenericExample
+{
+    class Client
+    {
+        private readonly Facade _facade;
+
+        public Client()
+        {
+            _facade = new Facade(new Subsystem1(), new Subsystem2());
+        }
+        public void ComplexOperation()
+        {
+            Console.Write(_facade.Operation());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.ComplexOperation();
+        }
+    }
+}
