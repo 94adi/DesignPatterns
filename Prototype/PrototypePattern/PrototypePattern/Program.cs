6de525fff0678b557fee40eaceb28bc4e87a8a0d
94adi <adrianrcotuna@gmail.com>
2021-07-15 10:20:08 +0300

    Prototype pattern

--- Prototype/PrototypePattern/PrototypePattern/Program.cs
+++ Prototype/PrototypePattern/PrototypePattern/Program.cs
@@ -31,21 +31,23 @@ namespace PrototypePatternCloneable
     {
         public readonly string[] Names;
         public readonly Address Address;
+        public int Age;
 
-        public Person(string[] names, Address address)
+        public Person(string[] names, Address address, int age)
         {
             Names = names;
             Address = address;
+            Age = age;
         }
 
         public override string ToString()
         {
-            return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Address)}: {Address}";
+            return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Age)}: {Age}, {nameof(Address)}: {Address}";
         }
 
         public object Clone()
         {
-            return new Person(Names, Address);
+            return new Person(Names, Address, Age);
         }
     }
 
@@ -53,7 +55,7 @@ namespace PrototypePatternCloneable
     {
         static void Main()
         {
-            var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+            var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123),12);
 
             var jane = (Person)john.Clone();
             jane.Address.HouseNumber = 321; // oops, John is now at 321
@@ -63,7 +65,7 @@ namespace PrototypePatternCloneable
 
             // but clone is typically shallow copy
             jane.Names[0] = "Jane";
-
+            jane.Age = 18;
             WriteLine(john);
             WriteLine(jane);
         }
---
+++ Prototype/PrototypePattern/PrototypePatternCopyConstructor/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using static System.Console;
+
+namespace PrototypePatternCopyConstructor
+{
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public Address(Address other)
+        {
+            StreetName = other.StreetName;
+            HouseNumber = other.HouseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+
+    public class Person 
+    {
+        public  string[] Names;
+        public readonly Address Address;
+        public int Age;
+
+        public Person(string[] names, Address address, int age)
+        {
+            Names = names;
+            Address = address;
+            Age = age;
+        }
+
+        public Person(Person other)
+        {
+            Names = other.Names ;
+            Address = new Address(other.Address);
+            Age = other.Age;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Age)}: {Age}, {nameof(Address)}: {Address}";
+        }
+
+    }
+
+    public static class Demo
+    {
+        static void Main()
+        {
+            var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123), 12);
+
+            var jane = new Person(john);
+            jane.Address.HouseNumber = 321;
+            jane.Address.StreetName = "New address";
+            jane.Names = new[] { "Jane", "Smith" };
+            jane.Age = 18;
+
+            WriteLine(john);
+            WriteLine(jane);
+        }
+    }
+}
