42ff138a5f6e045be0561f2667450cb6e4a3af03
Adrian Cotuna <adrianrcotuna@gmail.com>
2021-10-07 16:50:24 +0300

    function pointer | call conv code

--- FunctionPointer/TestPerformance/ConsoleApp3/Program.cs
+++ FunctionPointer/TestPerformance/ConsoleApp3/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApp3
 {
@@ -13,7 +15,7 @@ namespace ConsoleApp3
 
             ts.Start();
 
-            for (int i = 0; i < 10_000; i++)
+            for (int i = 0; i < 100_000; i++)
             {
                 PerformanceTest.UseFunctionPointer();
             }
@@ -22,13 +24,13 @@ namespace ConsoleApp3
 
             Console.WriteLine("Time taken: " + ts.ElapsedMilliseconds + " milliseconds");
 
-
+            ts.Reset();
 
             Console.WriteLine("Testing delegates performance");
 
             ts.Start();
 
-            for (int i = 0; i < 10_000; i++)
+            for (int i = 0; i < 100_000; i++)
             {
                 PerformanceTest.UseDelegate();
 
@@ -46,14 +48,26 @@ namespace ConsoleApp3
     {
         public delegate void Del();
 
-        public static void Log() 
-        { 
-            int a = 2; 
-            for (int i = 0; i < 1_000; i++) 
-            { 
-                a = 1; 
-            } 
+
+        //public static void Log() 
+        //{ 
+        //    int a = 2; 
+        //    for (int i = 0; i < 100_000; i++) 
+        //    { 
+        //        a = 1; 
+        //    } 
+        //}
+
+        [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static void Log()
+        {
+            int a = 2;
+            for (int i = 0; i < 100_000; i++)
+            {
+                a = 1;
+            }
         }
+       
         public static void Log(string p1) { Console.WriteLine("Log p1: " + p1); }
         public static void OtherLog(string p1) { Console.WriteLine(p1); }
         public static void Log(int i) { }
@@ -67,18 +81,18 @@ namespace ConsoleApp3
         //    }
         //}
 
-        //public static void TestHello(string msg)
-        //{
-        //    int a = 2; 
-        //    for (int i = 0; i < 1_000_000; i++) 
-        //    { 
-        //        a = 1; 
-        //    }
-        //}
+        public static void TestDelegate()
+        {
+            int a = 2;
+            for (int i = 0; i < 100_000; i++)
+            {
+                a = 1;
+            }
+        }
 
         public static void UseFunctionPointer()
         {
-            delegate*<void> a1 = &Log; // Log()
+            delegate* unmanaged[Cdecl]<void> a1 = &Log; // Log()
             a1();
             //delegate*<string, void> a2 = &Log; // Log(int i)
             //a2("hello");
@@ -87,7 +101,7 @@ namespace ConsoleApp3
         public static void UseDelegate()
         {
 
-            Del del1 = Log;
+            Del del1 = TestDelegate;
             //del1 += TestHello;
 
             del1();
