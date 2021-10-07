ee43c3e4c8f078c355450d9418fd0076869185b4
Adrian Cotuna <adrianrcotuna@gmail.com>
2021-10-07 15:21:19 +0300

    function pointer | performance test 3

--- FunctionPointer/TestPerformance/ConsoleApp3/Program.cs
+++ FunctionPointer/TestPerformance/ConsoleApp3/Program.cs
@@ -5,70 +5,95 @@ namespace ConsoleApp3
 {
     class Program
     {
-        public delegate void Del(string message);
-
         static void Main(string[] args)
         {
             var ts = new Stopwatch();
 
+            Console.WriteLine("Testing function pointers performance");
+
             ts.Start();
 
-            //for (long i = 0; i < 10_000; i++)
-            //{
-            //    Util.Use();
-            //}
-
-            for (long i = 0; i < 10_000; i++)
+            for (int i = 0; i < 10_000; i++)
             {
-
-                Del del1 = SayHello;
-
-                del1 += TestHello;
-
-                del1("geag");
+                PerformanceTest.UseFunctionPointer();
             }
-
 
             ts.Stop();
 
             Console.WriteLine("Time taken: " + ts.ElapsedMilliseconds + " milliseconds");
 
-            //Del del1 = SayHello;
 
-            //del1 += TestHello;
 
-            //del1("geag");
+            Console.WriteLine("Testing delegates performance");
+
+            ts.Start();
+
+            for (int i = 0; i < 10_000; i++)
+            {
+                PerformanceTest.UseDelegate();
+
+            }
+
+            ts.Stop();
+
+            Console.WriteLine("Time taken: " + ts.ElapsedMilliseconds + " milliseconds");
+
         }
 
-        static void SayHello(string msg)
-        {
-            Console.WriteLine("Say: " + msg);
-        }
-
-        static void TestHello(string msg)
-        {
-            Console.WriteLine("hello");
-        }
     }
 
-    unsafe class Util
+    unsafe class PerformanceTest
     {
-        public static void Log() { Console.WriteLine("Log simple"); }
+        public delegate void Del();
+
+        public static void Log() 
+        { 
+            int a = 2; 
+            for (int i = 0; i < 1_000; i++) 
+            { 
+                a = 1; 
+            } 
+        }
         public static void Log(string p1) { Console.WriteLine("Log p1: " + p1); }
         public static void OtherLog(string p1) { Console.WriteLine(p1); }
         public static void Log(int i) { }
 
-        public static void Use()
+        public static void SayHello()
+        {
+            int a = 2; 
+            for (int i = 0; i < 1_000; i++) 
+            { 
+                a = 1; 
+            }
+        }
+
+        //public static void TestHello(string msg)
+        //{
+        //    int a = 2; 
+        //    for (int i = 0; i < 1_000_000; i++) 
+        //    { 
+        //        a = 1; 
+        //    }
+        //}
+
+        public static void UseFunctionPointer()
         {
             delegate*<void> a1 = &Log; // Log()
             a1();
-            delegate*<string, void> a2 = &Log; // Log(int i)
-            a2("hello");
-            
-            
-
-            // Error: ambiguous conversion from method group Log to "void*"
-            //void* v = &Log;
+            //delegate*<string, void> a2 = &Log; // Log(int i)
+            //a2("hello");
         }
+
+        public static void UseDelegate()
+        {
+
+            Del del1 = SayHello;
+            //del1 += TestHello;
+
+            del1();
+        }
+
     }
+
 }
+
