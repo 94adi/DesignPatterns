fca2d344be2ee03bcd81948165399139baa5c146
94adi <adrianrcotuna@gmail.com>
2022-02-02 23:37:04 +0200

    Mediator Pattern

---
+++ Mediator/Mediator/MediatR/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using MediatR;
+
+namespace MediatRExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
